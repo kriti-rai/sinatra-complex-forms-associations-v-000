f48757d2bdbd94482cc1d384682f5ce6cc4eedec
Kriti Rai <kriti.rai@gmail.com>
2018-05-28 05:56:27 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,6 +33,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do
+    binding.pry
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
     if !params["owner"]["name"].empty?
